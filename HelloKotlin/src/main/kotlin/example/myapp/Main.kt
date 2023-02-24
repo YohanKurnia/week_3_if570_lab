0489144173700876fb6bace49e981a27f40f3f2e
YohanKurnia <yohankw7@gmail.com>
2023-02-24 20:57:09 +0700

    Adding class constructor

--- HelloKotlin/src/main/kotlin/example/myapp/Main.kt
+++ HelloKotlin/src/main/kotlin/example/myapp/Main.kt
@@ -1,10 +1,10 @@
 package example.myapp
 
 fun buildAquarium() {
-    val myAquarium = Aquarium()
-    myAquarium.printSize()
-    myAquarium.height = 60
-    myAquarium.printSize()
+    val aquarium6 = Aquarium(numberOfFish = 29)
+    aquarium6.printSize()
+    aquarium6.volume = 70
+    aquarium6.printSize()
 }
 
 fun main() {
